b2eca879e637226ba29544480a51566e5a29a7b1
yusraimran2 <yusraimran12@hotmail.com>
2024-11-12 21:15:52 +0500

    Add files via upload

---
+++ Q14.ts
@@ -0,0 +1,9 @@
+// 14: Guest List:If you could invite,living or deceased,to dinner who would you like to invite?Make a list that includes at least three people you'd like to invite to dinner,then ue your list to print a message to each person, inviting them to dinner
+
+let guestArr:string[] = ["Sana", "Sumera", "Afshan", "Umaima", "Sadaf"]
+
+console.log("Dear I you to dinner", guestArr[0])
+console.log("Dear I you to dinner", guestArr[1])
+console.log("Dear I you to dinner", guestArr[2])
+console.log("Dear I you to dinner", guestArr[3])
+console.log("Dear I you to dinner", guestArr[4])
